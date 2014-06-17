39e7ada0900a0cab295fdf2ec2bc790c53611125
Vongo <adelmarre@vongo.eu>
2014-06-17 15:05:57 +0200

    Read and load the data.

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,33 @@
+# You might have to uncomment the following in order to run the code.
 # install.packages('downloader')
+# install.packages('data.table')
+
+# Require packages
+require(data.table)
 require(downloader)
 
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-tmp <- tempfile(fileext = ".zip")
-download(url, tmp)
-dir <- unzip(tmp)
+# Define constants
+dirName <- "UCI HAR Dataset"
+trainPath <- "train/"
+testPath <- "test/"
+
+# Download the data from distant repo if the proper filder it is not in the working directory
+if(sum(grepl(dirName, list.files("."))) == 0){
+	url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+	tmp <- tempfile(fileext = ".zip")
+	download(url, tmp)
+	dir <- unzip(tmp)
+	# Make sure the newly created directory has the right name
+	dirName <- strsplit(dir[1], '/')[[1]][2]
+}
+
+path <- paste("./", dirName, "/", sep = "")
+
+# Load the data into data.table objects wisely named ;)
+xTrain <- read.table(paste(path, trainPath, "X_train.txt", sep = ""))
+yTrain <- read.table(paste(path, trainPath, "y_train.txt", sep = ""))
+xTest <- read.table(paste(path, testPath, "X_test.txt", sep = ""))
+yTest <- read.table(paste(path, testPath, "y_test.txt", sep = ""))
+subjectTrain <- read.table(paste(path, trainPath, "subject_train.txt", sep = ""))
+subjectTest <- read.table(paste(path, testPath, "subject_test.txt", sep = ""))
 
