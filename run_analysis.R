b18a7efc56b5a6955a204c3fb76d36c60531ade9
Vongo <adelmarre@vongo.eu>
2014-06-18 17:34:52 +0200

    Extracted mean and standard deviation. Ordered output for tidy data (mostly cosmetics). Fixed labels. Typos in comments.

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-### 0) Download and read the data
+### 0) Download, read and prepare the data
 
 # You might have to uncomment the following in order to run the code.
 # install.packages('downloader')
@@ -13,7 +13,7 @@ dirName <- "UCI HAR Dataset"
 trainPath <- "train/"
 testPath <- "test/"
 
-# Download the data from distant repo if the proper filder it is not in the working directory
+# Download the data from distant repo if the proper folder it is not in the working directory
 if(sum(grepl(dirName, list.files("."))) == 0){
 	url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 	tmp <- tempfile(fileext = ".zip")
@@ -25,7 +25,7 @@ if(sum(grepl(dirName, list.files("."))) == 0){
 
 path <- paste("./", dirName, "/", sep = "")
 
-# Load the data into data.table objects wisely named ;)
+# Load the data into wisely named objects ;)
 xTrain <- read.table(paste(path, trainPath, "X_train.txt", sep = ""))
 yTrain <- read.table(paste(path, trainPath, "y_train.txt", sep = ""))
 xTest <- read.table(paste(path, testPath, "X_test.txt", sep = ""))
@@ -33,8 +33,6 @@ yTest <- read.table(paste(path, testPath, "y_test.txt", sep = ""))
 subjectTrain <- read.table(paste(path, trainPath, "subject_train.txt", sep = ""))
 subjectTest <- read.table(paste(path, testPath, "subject_test.txt", sep = ""))
 
-### 3) Uses descriptive activity names to name the activities in the data set
-### 4) Appropriately labels the data set with descriptive variable names. 
 ## Load & fill variable names
 # Load
 names <- read.table(paste(path, "features.txt", sep=""))
@@ -60,5 +58,23 @@ xMeanStd <- xD[,c(grep("mean\\(\\)", names),grep("std\\(\\)", names))]
 theOneTrueData <- cbind(yD,subjectD,xMeanStd)
 
 ### 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+DT <- data.table(theOneTrueData)
+output <- as.data.frame(DT[,lapply(.SD, mean), by = c("Activity","Subject")])
+
+### 3) Uses descriptive activity names to name the activities in the data set
+### 4) Appropriately labels the data set with descriptive variable names.
+# note that 90% of the job was done before merging the data.
+output <- merge(output,activityLabels,by.x = "Activity",by.y = "Id")
+
 ### 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
+
+
+
+# Make it pretty and export
+# Remove the activity Id (cosmetics)
+output <- output[,-1]
+# Put the activity label in the first row
+output <- output[,c(ncol(output),1:(ncol(output)-1))]
+
+write.table(output,paste(dataPath, "meanStdTidyData.txt", sep=""),row.names = FALSE)
